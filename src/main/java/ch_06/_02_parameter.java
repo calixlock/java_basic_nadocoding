8933a4b742ccc1593a20c794ca1d0e439622c7ba
calixlock <calixlock@naver.com>
2023-05-02 17:06:37 +0900

    java method update

--- src/main/java/ch_06/_02_parameter.java
+++ src/main/java/ch_06/_02_parameter.java
@@ -1,13 +1,29 @@
 package ch_06;
 
 public class _02_parameter {
+    // main
     public static void main(String[] args) {
 
         int result = getPow(2);
         System.out.println(result);
         System.out.println(getPow(3));
+        System.out.println(getPow("4"));
+        System.out.println(getPow(2,5));
     }
+
+    // method
     public static int getPow(int num){
-        return num * num;
+        return getPow(num,2);
+    }
+    // method overloading
+    public static int getPow(String strNum){
+       int num = Integer.parseInt(strNum);
+        return getPow(num,2);
+    }
+    public static int getPow(int num, int exp){
+        int result = 1;
+        for (int i = 0; i < exp; i++) {
+            result *= num;
+        }return result;
     }
 }
